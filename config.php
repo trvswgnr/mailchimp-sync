5dc4ff453200d6a31f037f82690f3f7b726f3fa7
trav <trvswgnr@gmail.com>
2020-08-05 00:04:06 -0400

    formatting

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 
 // get data files.
 define( 'MCSYNC_DIR', dirname( __FILE__ ) . '/' );
-define( 'API_KEY', file_get_contents(MCSYNC_DIR . 'data/key.data') );
+define( 'API_KEY', file_get_contents( MCSYNC_DIR . 'data/key.data' ) );
 define( 'API_DC', substr( API_KEY, strpos( API_KEY, '-' ) + 1 ) );
 define( 'API_URL', 'https://' . API_DC . '.api.mailchimp.com/3.0/' );
-define( 'MCSYNC_LIST', file_get_contents(MCSYNC_DIR . 'data/list.data') );
+define( 'MCSYNC_LIST', file_get_contents( MCSYNC_DIR . 'data/list.data' ) );
